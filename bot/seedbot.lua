20162e4e4068149b8b2bb7ba69c9030c64f7618b
mahdisalar <mesti867@gmail.com>
2016-04-03 18:45:18 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {109645387},--Sudo users
+    sudo_users = {109645387,198008164},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
